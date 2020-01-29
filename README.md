b1994350515baa450b77737edd1c7b6920304696
Andrei Reinus <andrei.reinus@gmail.com>
2020-01-29 08:57:06 +0200

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# aspnet_slowapp
-Example application that could be used to test infrastructure resource usage
+# ASP.NET
+
+Example application that could be used to test infrastructure resource usage.
+
+Current implementation includes only CPU bound operation where application will try to compress random byte array.
+
+/compress - Compresses 10Mb 
+/compress/1024 - Compresses 1024 bytes
---
+++ src/Aspnet.SlowApp/Controllers/SlowController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aspnet.SlowApp.Controllers
+{
+    [ApiController]
+    public class SlowController : ControllerBase
+    {
+        [HttpGet]
+        [Route("compress")]
+        public Task<IActionResult> Compress()
+        {
+            return Compress(10 * 1024 * 1024);
+        }
+
+        [HttpGet]
+        [Route("compress/{byteCount}")]
+        public async Task<IActionResult> Compress(int byteCount)
+        {
+            byte[] bytes = new byte[byteCount];
+
+            using var randomGenerator = new RNGCryptoServiceProvider();
+            randomGenerator.GetBytes(bytes);
+
+            using var inputStream = new MemoryStream();
+            await inputStream.WriteAsync(bytes, 0, bytes.Length);
+            inputStream.Position = 0;
+
+            using var outputStream = new MemoryStream();
+            using (var zipStream = new GZipStream(outputStream, CompressionLevel.Optimal))
+            {
+                await inputStream.CopyToAsync(zipStream);
+            };
+
+            var results = outputStream.ToArray();
+
+            return Ok(new
+            {
+                hash = ComputeHash(results)
+            });
+        }
+
+
+        static string ComputeHash(byte[] bytes)
+        {
+            using var hash = SHA1.Create();
+
+            byte[] result = hash.ComputeHash(bytes);
+
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                builder.Append(result[i].ToString("x2"));
+            }
+            return builder.ToString();
+        }
+    }
+}
